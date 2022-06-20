ec3bbafcb15cd1afbe15ac1dee6054ab9c8bf185
wiekusviljoen <wiekusviljoen@gmail.com>
2022-06-20 16:29:08 +0200

    commit on master

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,37 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = [
+    'P',
+    'e',
+    't',
+    'r',
+    'u',
+    's',
+    'V',
+    'i',
+    'l',
+    'j',
+    'o',
+    'e',
+    'n',
+  ]
+  const jobArray = ['D', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
+
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-          Hi, <br /> I am Petrus Viljoen
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
           <br />
           Web Developer
         </h1>
