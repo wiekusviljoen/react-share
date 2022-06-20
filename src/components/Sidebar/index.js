58ad79c0132105a7282abf7a23428e9d456c337c
wiekusviljoen <wiekusviljoen@gmail.com>
2022-06-20 14:01:13 +0200

    commit on master

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,17 +1,13 @@
 import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
-import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
+
 import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
   <div className="nav-bar">
-    <Link className="logo" to="/">
-      <img src={LogoS} alt="logo" />
-      <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
-    </Link>
+    <Link className="logo" to="/"></Link>
     <nav>
       <NavLink exact="true" activeclassname="active" to="/">
         <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
