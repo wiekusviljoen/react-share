c8c1b20906e0146638c6845838c9384aa644dd42
wiekusviljoen <wiekusviljoen@gmail.com>
2022-06-20 15:35:25 +0200

    commit on master

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,19 @@
+import { Link } from 'react-router-dom'
+import './index.scss'
+
 const Home = () => {
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-          Hi, <br /> I am
+          Hi, <br /> I am Petrus Viljoen
+          <br />
+          Web Developer
         </h1>
+        <h2>Frontend Developer / Html / CSS / Javascript</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
       </div>
     </div>
   )
