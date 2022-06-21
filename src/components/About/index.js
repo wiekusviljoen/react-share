6d718a82038b12e0a098fc7e9a695eba025bb98c
wiekusviljoen <wiekusviljoen@gmail.com>
2022-06-21 11:20:17 +0200

    commit on master

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -11,9 +11,12 @@ const About = () => {
             idx={15}
           />
         </h1>
-        <p>I am ambitious and creative</p>
-        <p></p>
-        <p></p>
+        <p>
+          I am ambitious and creative. I am a team player and I also treat my
+          team with respect.
+        </p>
+        <p>I am good with time management and I am goal driven.</p>
+        <p>I work well under pressure.</p>
       </div>
     </div>
   )
