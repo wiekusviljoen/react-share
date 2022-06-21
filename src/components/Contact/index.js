6210b3ce66a9ba3537694928a0164fd05f9abd1d
wiekusviljoen <wiekusviljoen@gmail.com>
2022-06-21 14:26:50 +0200

    commit on master

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,20 @@
 import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 
 const Contact = () => {
   return (
     <>
-      <div className="containe contact-page"></div>
+      <div className="containe contact-page">
+        <div className="text-zone ">
+          <h1>
+            <AnimatedLetters
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'M', 'e']}
+              idx={15}
+            />
+          </h1>
+        </div>
+      </div>
       <Loader type="pacman" />
     </>
   )
