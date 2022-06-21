29f4354ab360a976267fad27ddcb32cf443ce102
wiekusviljoen <wiekusviljoen@gmail.com>
2022-06-21 14:04:26 +0200

    commit on master

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,52 +8,55 @@ import {
   faJsSquare,
   faReact,
 } from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
 
 const About = () => {
   return (
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
-            idx={15}
-          />
-        </h1>
-        <p>
-          I am ambitious and creative. I am a team player and I also treat my
-          team with respect. I am good with time management and I am goal
-          driven.
-        </p>
-        <p>
-          I work well under pressure. I can take criticism well and improve
-          where is expected.
-        </p>
-        <p>
-          I am looking for a job as a frontend developer. I would be honored to
-          work for your company.
-        </p>
-      </div>
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I am ambitious and creative. I am a team player and I also treat my
+            team with respect. I am good with time management and I am goal
+            driven.
+          </p>
+          <p>
+            I work well under pressure. I can take criticism well and improve
+            where is expected.
+          </p>
+          <p>
+            I am looking for a job as a frontend developer. I would be honored
+            to work for your company.
+          </p>
+        </div>
 
-      <div className="stage-cube-cont">
-        <div className="cubespinner">
-          <div className="face2">
-            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-          </div>
+        <div className="stage-cube-cont">
           <div className="cubespinner">
-            <div className="face3">
-              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
             </div>
             <div className="cubespinner">
-              <div className="face4">
-                <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+              <div className="face3">
+                <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
               </div>
               <div className="cubespinner">
-                <div className="face5">
-                  <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+                <div className="face4">
+                  <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
                 </div>
                 <div className="cubespinner">
-                  <div className="face1">
-                    <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+                  <div className="face5">
+                    <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+                  </div>
+                  <div className="cubespinner">
+                    <div className="face1">
+                      <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+                    </div>
                   </div>
                 </div>
               </div>
@@ -61,7 +64,8 @@ const About = () => {
           </div>
         </div>
       </div>
-    </div>
+      <Loader type="pacman" />
+    </>
   )
 }
 
