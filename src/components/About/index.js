a7218b73ef0fee80d49597fe3020e4789248f64d
wiekusviljoen <wiekusviljoen@gmail.com>
2022-06-21 12:34:08 +0200

    commit on master

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,29 +1,69 @@
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
 
 const About = () => {
   return (
     <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
-            idx={15}
-          />
-        </h1>
-        <p>
-          I am ambitious and creative. I am a team player and I also treat my
-          team with respect. I am good with time management and I am goal
-          driven.
-        </p>
-        <p>
-          I work well under pressure. I can take criticism well and improve
-          where is expected
-        </p>
-        <p>
-          I am looking for a job as a frontend developer. I would be honored to
-          work for your company.
-        </p>
+      <div className="text-zone">-</div>
+
+      <div className="stage-cube-cont">
+        <div className="cubespinner">
+          <div className="face1">
+            <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+          </div>
+          <div className="face2">
+            <FontAwesomeIcon icon={faHtml5} color="#DD0031" />
+          </div>
+          <div className="cubespinner">
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#DD0031" />
+            </div>
+            <div className="cubespinner">
+              <div className="face4">
+                <FontAwesomeIcon icon={faReact} color="#DD0031" />
+              </div>
+              <div className="cubespinner">
+                <div className="face5">
+                  <FontAwesomeIcon icon={faJsSquare} color="#DD0031" />
+                </div>
+                <div className="cubespinner">
+                  <div className="face1">
+                    <FontAwesomeIcon icon={faGitAlt} color="#DD0031" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <h1>
+            <AnimatedLetters
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I am ambitious and creative. I am a team player and I also treat my
+            team with respect. I am good with time management and I am goal
+            driven.
+          </p>
+          <p>
+            I work well under pressure. I can take criticism well and improve
+            where is expected
+          </p>
+          <p>
+            I am looking for a job as a frontend developer. I would be honored
+            to work for your company.
+          </p>
+        </div>
       </div>
     </div>
   )
