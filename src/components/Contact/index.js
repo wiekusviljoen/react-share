8abace50a0e5770e4d8d0157403d8ee2c4d9fec6
wiekusviljoen <wiekusviljoen@gmail.com>
2022-06-21 14:40:31 +0200

    commit on master

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -13,6 +13,35 @@ const Contact = () => {
               idx={15}
             />
           </h1>
+          <p>Please do not hesitate to contact me.</p>
+          <div className="contact-form">
+            <form>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea></textarea>
+                </li>
+              </ul>
+            </form>
+          </div>
         </div>
       </div>
       <Loader type="pacman" />
